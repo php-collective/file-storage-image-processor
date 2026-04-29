c08d5cb5f4e8709d540090b43879abaa4931e258
mscherer <dereuromark@web.de>
2026-04-29 02:49:57 +0200

    Use minimal author header on the new Driver enum file

--- src/Driver.php
+++ src/Driver.php
@@ -1,14 +1,10 @@
 <?php declare(strict_types = 1);
 
 /**
- * Copyright (c) Florian Krämer (https://florian-kraemer.net)
  * Licensed under The MIT License
  * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
  *
- * @copyright Copyright (c) Florian Krämer (https://florian-kraemer.net)
- * @author Florian Krämer
- * @link https://github.com/Phauthentic
+ * @author Mark Scherer
  * @license https://opensource.org/licenses/MIT MIT License
  */
 
